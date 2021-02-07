d5b3e3217a1b2a8219e358696e63a23f68849fe9
Krysl <krysl@qq.com>
2021-02-08 02:44:32 +0800

    change to use Product ver Vue; add license
    Signed-off-by: Krysl <krysl@qq.com>

--- config/metadata.js
+++ config/metadata.js
@@ -4,6 +4,7 @@ module.exports = {
   name: 'WebSwitcher: 在百度、谷歌之间切换搜索结果',
   namespace: 'https://github.com/Krysl',
   description: '为百度、谷歌添加按钮, 点击跳转到对方页面搜索同样内容',
+  license: pkg.license,
   version: pkg.version,
   author: pkg.author,
   source: pkg.repository.url,
@@ -17,11 +18,12 @@ module.exports = {
     `https://cdn.jsdelivr.net/npm/jquery@${pkg.dependencies.jquery}/dist/jquery.min.js`,
     `https://cdn.jsdelivr.net/npm/axios@${pkg.dependencies.axios}/dist/axios.min.js`,
     `https://cdn.jsdelivr.net/npm/axios-userscript-adapter@${pkg.dependencies['axios-userscript-adapter']}/dist/axiosGmxhrAdapter.min.js`,
-    `https://cdn.jsdelivr.net/npm/vue@${pkg.dependencies.vue}/dist/vue.global.js`,
-    `https://cdn.jsdelivr.net/npm/vue-class-component@${pkg.dependencies['vue-class-component']}/dist/vue-class-component.global.js`,
+    `https://cdn.jsdelivr.net/npm/vue@${pkg.dependencies.vue}/dist/vue.global.prod.js`,
+    `https://cdn.jsdelivr.net/npm/vue-class-component@${pkg.dependencies['vue-class-component']}/dist/vue-class-component.global.prod.js`,
     `https://cdn.jsdelivr.net/npm/@svgdotjs/svg.js@${pkg.dependencies['@svgdotjs/svg.js']}/dist/svg.min.js`,
     // `https://cdn.jsdelivr.net/npm/base64-js@${pkg.dependencies['base64-js']}/index.js`,
   ],
+  supportURL: pkg.repository.url,
   grant: [
     'GM_xmlhttpRequest'
   ],
